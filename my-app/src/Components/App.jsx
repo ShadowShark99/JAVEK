68193c006ebc843c3fb69d020c76729b2ce06642
Eucharist Tan <Euki16321@gmail.com>
2024-11-16 16:15:58 -0600

    Changed title

---
+++ my-app/src/Components/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <div className="App">
+         <Header />
+         <Note />
+          <Footer/>
+      </div>
+    );
+  }
+  
+  export default App;
+  
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <h1 className = "title">
+      Hello My name is Eucharist Tan and today I am going to improve your productivity
+    </h1>
   </React.StrictMode>
 );
 
